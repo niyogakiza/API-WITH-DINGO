46cee5a280b5e580f070327a8fb67195db2c4c5b
Aimable <aimable.niyogakiza@drpgroup.com>
2019-08-11 13:07:21 +0100

    Services and custom php artisan command for query

---
+++ app/Services/QueryService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+use App\Models\City;
+
+class QueryService
+{
+    protected $openWeatherMapService;
+    protected $apixuService;
+    public function __construct(
+        OpenWeatherMapService $openWeatherMapService,
+        ApixuService $apixuService
+    ) {
+        $this->apixuService = $apixuService;
+        $this->openWeatherMapService = $openWeatherMapService;
+    }
+    public function queryAll()
+    {
+        $cities = City::all();
+        $this->openWeatherMapService->query(env('OPENWEATHERMAP_APIKEY'), $cities);
+        $this->apixuService->query(env('APIXU_APIKEY'), $cities);
+    }
+}
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 ### Dingo API - https://github.com/dingo/api
 ### Laravel CORS - https://github.com/barryvdh/laravel-cors
 ### Ide-helper - https://github.com/barryvdh/laravel-ide-helper
+### Guzzlehttp - https://github.com/guzzle/guzzle
